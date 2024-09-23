0164bbcd3b8287265465730cc3d82ee53cf4831f
SergioClaustro <sergio.claustro5250@alumnos.udg.mx>
2024-09-22 19:58:52 -0600

    Update chat.js
    link de la web

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost:4000");
+var socket = io.connect("https://chat-node-js-c95z.onrender.com/");
 
 //Query DOM
 var output = document.getElementById("output"),
